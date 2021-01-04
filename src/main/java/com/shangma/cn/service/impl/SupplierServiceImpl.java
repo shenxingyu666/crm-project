d00d8da22b93d8482619254da23780bab6536eb2
shenxingyu <690540786@qq.com>
2021-01-04 20:24:30 +0800

    项目基础搭建完成

---
+++ src/main/java/com/shangma/cn/service/impl/SupplierServiceImpl.java
@@ -0,0 +1,11 @@
+package com.shangma.cn.service.impl;
+
+import com.shangma.cn.entity.Supplier;
+import com.shangma.cn.service.base.impl.BaseServiceImpl;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class SupplierServiceImpl extends BaseServiceImpl<Supplier,Long> {
+}
